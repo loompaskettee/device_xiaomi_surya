040f5de9bd8796cb5f86cdf60b73e7de6ccbc287
HeroBuxx <me@herobuxx.me>
2025-08-16 14:00:56 +0800

    surya: Enchanted Bloom
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- device.mk
+++ device.mk
@@ -279,7 +279,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay/no-rro
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
--- halcyon_surya.mk
+++ halcyon_surya.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Halcyon stuff.
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := halcyon_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 NFC
